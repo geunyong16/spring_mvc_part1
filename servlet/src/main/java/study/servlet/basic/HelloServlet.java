664e5866f5fc54fe9a887e20494eefd8b957b603
geunyong16 <joyonggeun16@gmail.com>
2025-07-16 20:23:10 +0900

    Initial

---
+++ servlet/src/main/java/study/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package study.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+
+        /// HttpServletRequest와 HttpServletResponse은 인터페이스이기 때문에 구현체가 출력될 것임
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("hello " + username);
+
+    }
+}
